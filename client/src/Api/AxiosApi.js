d5e43a430b30172b213a0ffb0e10b53203dc546a
ANIL NAYAK <goramnayak5@gmail.com>
2023-05-15 18:15:02 +0530

    making the code production ready

--- client/src/Api/AxiosApi.js
+++ client/src/Api/AxiosApi.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-
+const baseURL =
+  process.env.ENVIRONMENT === "development"
+    ? "http://localhost:3000/api/v1/todo"
+    : "/api/v1/todo";
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL,
 });
