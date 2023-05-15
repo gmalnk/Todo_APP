67bae7be82c78b873e484bc2845c2ef1dfccb192
ANIL NAYAK <goramnayak5@gmail.com>
2023-05-15 19:06:55 +0530

    solved the bug related to the environment variable

--- client/src/Api/AxiosApi.js
+++ client/src/Api/AxiosApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const baseURL =
-  process.env.ENVIRONMENT === "development"
-    ? "http://localhost:3000/api/v1/todo"
-    : "/api/v1/todo";
+  process.env.ENVIRONMENT === "production"
+    ? "/api/v1/todo"
+    : "http://localhost:3000/api/v1/todo";
 export default axios.create({
   baseURL,
 });
